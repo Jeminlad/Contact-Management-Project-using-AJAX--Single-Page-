082fa4000cc4c82da16762bb5020bbba03069b85
Jemin Lad <jeminlad2003@gmail.com>
2025-03-12 11:29:41 +0530

    Added new features

--- obj/Debug/net8.0/ContactProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/ContactProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContactProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+000f1c4f5311d025e7ae1ce3da0e64bb7adaacc2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+483d5737f73f81589d52dc58f22f2b9ca1545f3f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContactProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContactProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
